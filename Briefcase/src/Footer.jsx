dd2a6dd5e4b3e30f988a1e7d78f61ed1431c2972
SBdevz <sbaral.tech@gmail.com>
2026-09-04 17:18:17 -0500

    Footer.jsx

---
+++ Briefcase/src/Footer.jsx
@@ -0,0 +1,9 @@
+function footer () {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>&copy; Briefcase Pocket Lawyer. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
