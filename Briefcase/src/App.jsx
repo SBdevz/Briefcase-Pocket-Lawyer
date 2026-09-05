b803e95d84bfe5293e617d908831adcf7b4e8dd0
Crypto Chud <aryansharoragt@gmail.com>
2026-09-05 11:44:59 -0500

    About Implemented to Footer

--- Briefcase/src/App.jsx
+++ Briefcase/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
 import Card from './Card.jsx';
+import About from './About.jsx';
 
 function App() {
     if (window.location.pathname === '/About') {
