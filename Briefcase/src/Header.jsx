350b430abdc6372ec59f50c47409ae366876e59e
Crypto Chud <aryansharoragt@gmail.com>
2026-09-05 17:24:57 -0500

    Header.jsx Links Added

--- Briefcase/src/Header.jsx
+++ Briefcase/src/Header.jsx
@@ -4,6 +4,10 @@ function Header() {
             <h1>Briefcase Pocket Lawyer</h1>
             <nav>
                 <ul>
+                    <li>Home</li>
+                    <li>AI</li>
+                    <li>Sample 1</li>
+                    <li>Sample 2</li>
                 </ul>
             </nav>
         </header>
