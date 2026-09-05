2844cdee79c82f97810a34f600d43ad544a17dd7
SBdevz <sbaral.tech@gmail.com>
2026-09-05 17:15:31 -0500

    Bill of Rights Card Added

--- Briefcase/src/Card.jsx
+++ Briefcase/src/Card.jsx
@@ -2,6 +2,9 @@
 function Card(){
     return(
         <div className="card">
+            <img className="card-image" src="https://bri-wp-images.s3.amazonaws.com/wp-content/uploads/founding-fathers-550x271.png" alt="Bill of Rights" />
+            <br></br>
+            <br></br>
             <h2>Bill of Rights</h2>
             <p>The Bill of Rights is the first ten amendments to the United States Constitution. </p>   
             <p>It is important to know your rights and understand how they protect you as a citizen. </p>    
